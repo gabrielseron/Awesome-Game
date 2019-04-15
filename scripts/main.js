c58c8236ff78c06e45bf4eb4c1d878ec7d20b8d5
gabrielseron <47740013+gabrielseron@users.noreply.github.com>
2019-04-15 15:48:08 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,38 @@
-class Boss{
-  constructor(name, class, healthPoint, level, introSound, deathSound)
-    {
+class Item{
+  constructor(name, damage, multiplier, autoClickSpeed, upgradeMultiplierCost, upgradeAutoClickSpeedCost){
       this.name = name
-      this.class = class
-      this.healthPoint = healthPoint
+      this.damage = 1
+      this.multiplier = 1
+      this.autoClickSpeed = 0
+      this.upgradeMultiplierCost = x
+      this.upgradeAutoClickSpeedCost = x
+    }
+}
+
+class Boss{
+  constructor(name, title, healthPoint, level, introSound, deathSound, progress){
+      this.name = name
+      this.title = title
+      this.healthPoint = 100
       this.level = level
       this.introSound = introSound
       this.deathSound = deathSound
+      this.progress = document.querySelector('#avancement')
     }
 
     display()
     {
-      console.log(this.name, this.class, this.healthPoint, this.level, this.introSound, this.deathSound)
+      console.log(this.name, this.title, this.healthPoint, this.level, this.introSound, this.deathSound)
+      this.progress.setAttribute('value',this.healthPoint )
+      this.progress.setAttribute('max',this.healthPoint )
     }
   }
 
-    //let bourdin = new Boss('J.J.Boudin', 'Terreur des Politiques', 1000, 1, 'bourdinI.mp3', 'bourdinD.mp3')
-    
+
+let bigboss = new Boss('Hulk', 'test', 150, 1,'test', 'test')
+bigboss.display()
+
+function punchDamage(){
+  this.progress.setAttribute('value','value' )
+}
+document.getElementById("Punch").addEventListener("click", punchDamage());
