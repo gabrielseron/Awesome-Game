fda4de4d84c9b2e7ba1748760beec1522598eb4c
Anouk Hervouet <anouk.hervouet12@gmail.com>
2019-04-17 11:46:58 +0200

    update home

--- scripts/home.js
+++ scripts/home.js
@@ -7,5 +7,22 @@ button.appendChild(a)
 a.setAttribute('href','index.html')
 const text=document.createTextNode('Jouer')
 a.appendChild(text)
-button.appendChild(text)
-button.setAttribute("href","index.html")
+button.appendChild(a)
+a.setAttribute('href','index.html')
+console.log(a)
+/*
+function over()
+        {
+            document.getElementByTagName("button").style.transform="scale(1)";
+            document.getElementByTagName("button").style.transition=".2s ease-in-out";
+        }
+*/
+
+div.onmouseenter = function() {
+  document.getElementByName("button").style.transform="scale(1)";
+  document.getElementByName("button").style.transition=".2s ease-in-out";;
+}
+
+div.onmouseleave = function() {
+    document.getElementByName("button").style.transform="scale(1.3)";
+}
