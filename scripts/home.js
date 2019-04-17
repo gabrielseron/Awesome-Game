dce2229331993d239eb525203fbecb959ecb828e
Anouk Hervouet <anouk.hervouet12@gmail.com>
2019-04-17 13:54:22 +0200

    update

--- scripts/home.js
+++ scripts/home.js
@@ -7,22 +7,3 @@ play.setAttribute('')
 div.appendChild('button')
 const a=document.createElement('a')
 button.appendChild('a')
-
-
-/*
-function over()
-        {
-            document.getElementByTagName("button").style.transform="scale(1)";
-            document.getElementByTagName("button").style.transition=".2s ease-in-out";
-        }
-*/
-/*
-div.onmouseenter = function() {
-  document.getElementByName("button").style.transform="scale(1)";
-  document.getElementByName("button").style.transition=".2s ease-in-out";;
-}
-
-div.onmouseleave = function() {
-    document.getElementByName("button").style.transform="scale(1.3)";
-}
-*/
